4c2b5c01d4a47cd21d5cf827bc613088e3c8bfac
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-12-14 12:50:53 -0900

    hw update

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,48 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-public class Program
+namespace ToDoList
 {
-  private static bool isFirstTime = true;
-  public static void Main()
+  public class Program
   {
-    if(isFirstTime)
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to your To Do List!");
-      isFirstTime = false;
-    }
-    Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+      var host = new WebHostBuilder()
+          .UseKestrel()
+          .UseContentRoot(Directory.GetCurrentDirectory())
+          .UseIISIntegration()
+          .UseStartup<Startup>()
+          .Build();
 
-    string userInput = Console.ReadLine();
-
-    if(userInput == "Add" || userInput == "add")
-    {
-      Console.WriteLine("Please enter a description for the new item");
-      Item newItem = new Item(Console.ReadLine());
-      Console.WriteLine(newItem.Description + " has been added to your list");
-      Main();
+      host.Run();
     }
-    else if (userInput == "View" || userInput == "view")
-    {
-      if(Item.GetAll().Count == 0)
-      {
-        Console.WriteLine("... the list is empty...");
-      }
-      else 
-      {
-        foreach(Item item in Item.GetAll())
-        {
-          Console.WriteLine(item.Description);
-        }
-      }
-       Main();
-    }
-    else
-    {
-      Console.WriteLine("Please enter a valid selection");
-      Main();
-    }
-    
   }
 }
